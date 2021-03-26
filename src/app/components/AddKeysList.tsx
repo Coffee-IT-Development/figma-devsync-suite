5adce9c7d5849b6a14b88bc64c4cf077595a536a
Lex den Otter <denotter@coffeeit.nl>
2021-03-26 16:27:26 +0100

    styling add keys

--- src/app/components/AddKeysList.tsx
+++ src/app/components/AddKeysList.tsx
@@ -1,7 +1,17 @@
 import styled from "@emotion/styled";
 import React from "react";
+import { Checkbox, Icon, Input, Textarea } from "react-figma-plugin-ds";
 
-export const AddKeysList = ({ nodes }) => {
+type Props = {
+  nodes: Array<TextNode & { name: string; characters: string }>;
+  showIgnored: boolean;
+};
+
+export const AddKeysList = ({ nodes, showIgnored }: Props) => {
+  const filteredNodes = nodes
+    .filter((n) => !n.name?.startsWith("#"))
+    .filter((n) => (showIgnored ? n : !n.name?.startsWith("!")));
+
   const focusNodes = (name, content) => {
     const ids = nodes
       .filter((n) => n.name === name && n.characters === content)
@@ -24,14 +34,14 @@ export const AddKeysList = ({ nodes }) => {
     ) as HTMLInputElement).checked = !!value;
   };
 
-  const toggleIgnore = (id, name, content) => {
-    var element = document.getElementById(id);
+  const toggleIgnore = (id) => {
+    const elementId = id.split("ignoreicon")[1];
+    const element = document.getElementById(id);
 
     if (element.classList.contains("icon--visible")) {
-      element.classList.add("icon--hidden");
-      element.classList.remove("icon--visible");
+      element.classList.replace("icon--visible", "icon--hidden");
       const entries = nodes
-        .filter((n) => n.name === name && n.characters === content)
+        .filter((n) => n.id === elementId)
         .map((n) => ({ id: n.id, value: "!ignore" }));
 
       parent.postMessage(
@@ -44,10 +54,9 @@ export const AddKeysList = ({ nodes }) => {
         "*"
       );
     } else {
-      element.classList.add("icon--visible");
-      element.classList.remove("icon--hidden");
+      element.classList.replace("icon--hidden", "icon--visible");
       const entries = nodes
-        .filter((n) => n.name === name && n.characters === content)
+        .filter((n) => n.id === elementId)
         .map((n) => ({ id: n.id, value: "Ready to rename layers" }));
 
       parent.postMessage(
@@ -64,69 +73,49 @@ export const AddKeysList = ({ nodes }) => {
 
   return (
     <KeyList>
-      {nodes
-        .filter((node) => node.name === "#" || !node.name?.startsWith("#"))
-        .filter((node) => node.name === "#" || !node.name?.startsWith("!"))
-        .map((node) => {
-          const id = node.id.replace(":", "-");
-          const idIgnore = node.id
-            .replace(":", "_")
-            .replace(";", "_")
-            .replace(":", "_");
-          return (
-            <>
-              <Entry id={id}>
-                <div className="checkbox add_checkbox">
-                  <input
-                    id={`checkbox-${id}`}
-                    type="checkbox"
-                    className="checkbox__box"
+      {filteredNodes.map((node) => {
+        const id = node.id.replace(":", "-");
+        return (
+          <>
+            <Entry id={node.id} key={node.id}>
+              <Row>
+                <Checkbox label="" />
+                <InputContainer first={true}>
+                  <Input
+                    placeholder=""
+                    defaultValue={node.name}
+                    isDisabled
+                    icon="frame"
+                    type="text"
                   />
-                  <label
-                    htmlFor={`checkbox-${id}`}
-                    className="checkbox__label"
+                </InputContainer>
+                <InputContainer>
+                  <Input
+                    placeholder=""
+                    defaultValue={node.name}
+                    icon="swap"
+                    type="text"
                   />
-                  <div
-                    className="icon-button"
-                    onClick={() =>
-                      toggleIgnore(
-                        `ignoreicon${idIgnore}`,
-                        node.name,
-                        node.characters
-                      )
-                    }
-                  >
-                    <div
-                      id={`ignoreicon${idIgnore}`}
-                      className="icon icon--visible"
-                    />
-                  </div>
-                </div>
-                <div className="add_inputs">
-                  <div className="input input--with-icon add_layer_name">
-                    <div className="icon icon--frame" />
-                    <input
-                      className="input__field"
-                      value={node.name}
-                      disabled
-                    />
-                  </div>
-                  <div className="input input--with-icon add_layer_name_new">
-                    <div className="icon icon--swap" />
-                    <input
-                      className="input__field"
-                      value=""
-                      onFocus={() => focusNodes(node.name, node.characters)}
-                      onChange={(e) => check(id, e.currentTarget.value)}
-                    />
-                  </div>
-                  <StyledTextArea rows={2}>{node.characters}</StyledTextArea>
-                </div>
-              </Entry>
-              <div className="separator" />
-            </>
-          );
-        })}
+                </InputContainer>
+              </Row>
+              <Row>
+                <StyledIcon
+                  onClick={() => toggleIgnore(`ignoreicon${node.id}`)}
+                  name={node.name?.startsWith("!") ? "hidden" : "visible"}
+                />
+                <TextAreaContainer>
+                  <StyledTextArea
+                    placeholder={node.characters}
+                    isDisabled
+                    rows={2}
+                  />
+                </TextAreaContainer>
+              </Row>
+            </Entry>
+            <Seperator />
+          </>
+        );
+      })}
     </KeyList>
   );
 };
@@ -137,18 +126,43 @@ const KeyList = styled.ul`
   margin-top: 0;
 `;
 
-const Entry = styled.li`
-  height: 128px;
-  padding-top: 12px;
-
-  .icon-button {
-    position: absolute;
-    margin-top: 64px;
-  }
+const Seperator = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #e5e5e5;
 `;
 
-const StyledTextArea = styled.textarea`
-  font: 400 13.3333px Arial;
+const Entry = styled.li`
+  height: 128px;
+  padding: 12px 16px;
+`;
+
+const Row = styled.div`
+  display: flex;
+  width: 100%;
+  position: relative;
+`;
+
+const InputContainer = styled.div<{ first?: boolean }>`
+  display: flex;
+  width: 100%;
+  border: 1px solid rgb(0, 0, 0, 0.1);
+
+  ${(props) => props.first && `margin-right: 13px`};
+`;
+
+const StyledIcon = styled(Icon)`
+  background-color: "transparant" !important;
+`;
+
+const TextAreaContainer = styled.div`
+  display: flex;
+  width: 100%;
+  border: 1px solid rgb(0, 0, 0, 0.1);
+`;
+
+const StyledTextArea = styled(Textarea)`
+  font: 400 11px Arial;
   font-style: normal;
   font-variant-ligatures: normal;
   font-variant-caps: normal;
@@ -156,7 +170,7 @@ const StyledTextArea = styled.textarea`
   font-variant-east-asian: normal;
   font-weight: 400;
   font-stretch: normal;
-  font-size: 13.3333px;
+  font-size: 11px;
   line-height: normal;
   font-family: Arial;
 `;
